e7f48bdf428390d77b9e8f1d373eb5bbac72bdb4
kelvintrc <kvynmutwiri@gmail.com>
2022-06-02 09:32:55 +0000

    [Feature]: Importing a function from a file and printing the result

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = int(1)
+    b = int(2)
+    
+    sum = add(a, b)
+
+    print("{} + {} = {}".format(a, b, sum))
+
+
