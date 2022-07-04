4b1b2aa9a6da6d76e613f926c4c1d1be7ca164c7
kelvintrc <kvynmutwiri@gmail.com>
2022-07-04 13:00:57 +0000

    [Fix]: A function that returns a list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This module creates a function lookup """
+
+
 def lookup(obj):
     """ A function that returns the list of available attributes
     and methods of an object
