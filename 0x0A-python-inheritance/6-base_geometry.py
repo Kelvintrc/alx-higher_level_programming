4ddb6e3dc2e6c31b7642b7e844cea270d68be3b5
kelvintrc <kvynmutwiri@gmail.com>
2022-07-04 14:52:07 +0000

    [Feature]: A class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module defines a Class BaseGeometry """
+
+
+class BaseGeometry():
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
