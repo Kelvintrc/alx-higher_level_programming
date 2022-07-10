7e4931e74fb014132b853915ec0655acb218d824
kelvintrc <kvynmutwiri@gmail.com>
2022-07-10 11:33:45 +0000

    [Feature]: Class Rectangle that inherits from base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,11 +3,10 @@
 
 import unittest
 from models.base import Base
-from models.square import Square
-from models.rectangle import Rectangle
 from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
+from models.rectangle import Rectangle
 
 class TestBaseMethods(unittest.TestCase):
     """ Suite to test Base class """
