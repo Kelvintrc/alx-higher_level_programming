00c1fd4f0babb35f6bd027fe7f5b2e20c7a7df53
kelvintrc <kvynmutwiri@gmail.com>
2022-08-30 13:25:41 +0000

    [Feature]: Listing all records where name value is not empty

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records of a table
+-- Query to list all reords of the table second_table which have the name value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
